b7be43bd86f674c0cf5b8e7029ed479e45a42154
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-17 15:59:35 +0530

    Added startup()

--- nush.c
+++ nush.c
@@ -10,7 +10,7 @@ Developed by: Vigneshwar Ravichandar
 -----------------------------------------------------------
 */
 
-// INCLUDING REQUIRED HEADER FILES
+// HEADER FILES
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -19,5 +19,22 @@ Developed by: Vigneshwar Ravichandar
 #include<sys/wait.h>
 #include<readline/readline.h>
 #include<readline/history.h>
+
+// DEFINE AREA
+#define clear() printf("\033[H\033[J") 
+
+// FUNCTION FOR STARTUP MESSAGE
+void startup()
+{
+    clear();
+    printf("\nNuSH Shell is Started...");
+    char *username = getenv("USER");
+    printf("\nUser: @%s",username);
+    printf(']\n');
+    sleep(1);
+    clear();
+}
+
+
 
 
