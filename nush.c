22dccf23a04108827fb465a6d0a0689f625fda21
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-17 15:47:34 +0530

    Added required header files

--- nush.c
+++ nush.c
@@ -1,1 +1,23 @@
+/*
+-----------------------------------------------------------
+
+NUSH
+
+NuSH or Null Shell is an open source custom shell for UNIX-based Operating Systems.
+
+Developed by: Vigneshwar Ravichandar
+
+-----------------------------------------------------------
+*/
+
+// INCLUDING REQUIRED HEADER FILES
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/unistd.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
 
