da8596a2d1279540957b6d9444a8f1f51da3c590
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-20 15:01:38 +0530

    Changed cursor style

--- prompt.c
+++ prompt.c
@@ -3,10 +3,10 @@
 
 void print_prompt1(void)
 {
-    fprintf(stderr, "$ ");
+    fprintf(stderr, "/> ");
 }
 
 void print_prompt2(void)
 {
-    fprintf(stderr, "/> ");
+    fprintf(stderr, "$ ");
 }
