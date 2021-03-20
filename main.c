0d19b44e1aafe146981fed312bdce472c06c6874
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-20 13:48:29 +0530

    Some changes

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     {
         print_prompt1();
 
-        command = read_cmd();
+        command = read_command();
 
         if(!command)
         {
@@ -38,4 +38,59 @@ int main(int argc, char **argv)
     } while(1);
 
     exit(EXIT_SUCCESS);
+}
+
+char *read_command(void)
+{
+    char buffer[1024];
+    char *pointer = NULL;
+    char pointer_length = 0;
+
+    while(fgets(buffer, 1024, stdin))
+    {
+        int buffer_length = strlen(buffer);
+
+        if(!pointer)
+        {
+            pointer = malloc(buffer_length+1);
+        }
+        else
+        {
+            char *pointer2 = realloc(pointer, pointer_length+buffer_length+1);
+
+            if(pointer2)
+            {
+                pointer = pointer2;
+            }
+            else
+            {
+                free(pointer);
+                pointer = NULL;
+            }
+        }
+
+        if(!pointer)
+        {
+            fprintf(stderr, "Error: Failed to allocate Buffer: %s\n", strerror(errno));
+            return NULL;
+        }
+
+        strcpy(pointer+pointer_length, buffer);
+
+        if(buffer[buffer_length-1] == '\n')
+        {
+            if(buffer_length == 1 || buffer[buffer_length-2] != '\\')
+            {
+                return pointer;
+            }
+
+            pointer[pointer_length+buffer_length-2] = '\0';
+            buffer_length -= 2;
+            print_prompt2();
+        }
+
+        pointer_length += buffer_length;
+    }
+
+    return pointer;
 }
--- shell.h
+++ shell.h
@@ -4,6 +4,6 @@
 void print_prompt1(void);
 void print_prompt2(void);
 
-char *read_cmd(void);
+char *read_command(void);
 
 #endif
