304cac28f47a540507c0bfbc018a2de68390c8c4
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-21 13:14:54 +0530

    Some changes

--- parser.h
+++ parser.h
@@ -1,8 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "scanner.h"    /* struct token_s */
-#include "source.h"     /* struct source_s */
+#include "scanner.h" 
+#include "source.h"     
 
 struct node_s *parse_simple_command(struct token_s *tok);
 
