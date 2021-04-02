d39fd6991e2d03eccd58ff84d51126d1950038d2
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 12:00:15 +0530

    Added working directory option and coloring

--- src/prompt.c
+++ src/prompt.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "shell.h"
+#include <unistd.h>
+#include <limits.h>
+
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
 
 void print_prompt1(void)
 {
-    fprintf(stderr, "NUSH >$ ");
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    fprintf(stderr, CYN "NUSH > " WHT "%s" CYN "$ " RESET,cwd);
 }
 
 void print_prompt2(void)
 {
-    fprintf(stderr, "NUSH > ");
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    fprintf(stderr,CYN "NUSH > " WHT "%s" CYN "> " RESET,cwd);
 }
