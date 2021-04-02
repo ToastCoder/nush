26eb3e16b5fe6bad1e949e5e175d9aae88aae643
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 17:42:45 +0530

    Some changes

--- src/symtab/symtab.c
+++ src/symtab/symtab.c
@@ -79,3 +79,27 @@ void free_symtab(struct symtab_s *symtab)
 
     free(symtab);
 }
+
+void dump_local_symtab(void)
+{
+    struct symtab_s *symtab = symtab_stack.local_symtab;
+    int i = 0;
+    int indent = symtab->level * 4;
+
+    fprintf(stderr, "%*sSymbol table [Level %d]:\r\n", indent, " ", symtab->level);
+    fprintf(stderr, "%*s===========================\r\n", indent, " ");
+    fprintf(stderr, "%*s  No               Symbol                    Val\r\n", indent, " ");
+    fprintf(stderr, "%*s------ -------------------------------- ------------\r\n", indent, " ");
+
+    struct symtab_entry_s *entry = symtab->first;
+
+    while(entry)
+    {
+        fprintf(stderr, "%*s[%04d] %-32s '%s'\r\n", indent, " ",
+                i++, entry->name, entry->val);
+        entry = entry->next;
+    }
+
+    fprintf(stderr, "%*s------ -------------------------------- ------------\r\n", indent, " ");
+}
+
