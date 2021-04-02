c40c08dd777ded6999a98605077d7b09c5946958
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 17:37:36 +0530

    Added user visibility in terminal

--- src/prompt.c
+++ src/prompt.c
@@ -9,14 +9,18 @@
 
 void print_prompt1(void)
 {
+    char user[PATH_MAX];
     char cwd[PATH_MAX];
+    cuserid(user);
     getcwd(cwd, sizeof(cwd));
-    fprintf(stderr, CYN "NUSH > " WHT "%s" CYN "$ " RESET,cwd);
+    fprintf(stderr, CYN "NUSH@%s>" WHT "%s" CYN "$ " RESET,user,cwd);
 }
 
 void print_prompt2(void)
 {
+    char user[PATH_MAX];
     char cwd[PATH_MAX];
+    cuserid(user);
     getcwd(cwd, sizeof(cwd));
-    fprintf(stderr,CYN "NUSH > " WHT "%s" CYN "> " RESET,cwd);
+    fprintf(stderr,CYN "NUSH@%s>" WHT "%s" CYN "> " RESET,user,cwd);
 }
