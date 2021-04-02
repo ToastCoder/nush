2b90a8543ed43495d8a143e66faf3149c555b2c7
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 17:46:05 +0530

    Some changes

--- src/symtab/symtab.c
+++ src/symtab/symtab.c
@@ -266,3 +266,60 @@ void symtab_entry_setval(struct symtab_entry_s *entry, char *val)
     }
 }
 
+void symtab_stack_add(struct symtab_s *symtab)
+{
+    symtab_stack.symtab_list[symtab_stack.symtab_count++] = symtab;
+    symtab_stack.local_symtab = symtab;
+}
+
+
+struct symtab_s *symtab_stack_push(void)
+{
+    struct symtab_s *st = new_symtab(++symtab_level);
+    symtab_stack_add(st);
+    return st;
+}
+
+
+struct symtab_s *symtab_stack_pop(void)
+{
+    if(symtab_stack.symtab_count == 0)
+    {
+        return NULL;
+    }
+
+    struct symtab_s *st = symtab_stack.symtab_list[symtab_stack.symtab_count-1];
+
+    symtab_stack.symtab_list[--symtab_stack.symtab_count] = NULL;
+    symtab_level--;
+
+    if(symtab_stack.symtab_count == 0)
+    {
+        symtab_stack.local_symtab  = NULL;
+        symtab_stack.global_symtab = NULL;
+    }
+    else
+    {
+        symtab_stack.local_symtab = symtab_stack.symtab_list[symtab_stack.symtab_count-1];
+    }
+
+    return st;
+}
+
+
+struct symtab_s *get_local_symtab(void)
+{
+    return symtab_stack.local_symtab;
+}
+
+
+struct symtab_s *get_global_symtab(void)
+{
+    return symtab_stack.global_symtab;
+}
+
+
+struct symtab_stack_s *get_symtab_stack(void)
+{
+    return &symtab_stack;
+}
