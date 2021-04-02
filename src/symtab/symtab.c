4ddb6a6cdec5249fd1fb2331ae382d26c5ebdb09
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 17:43:52 +0530

    Some changes

--- src/symtab/symtab.c
+++ src/symtab/symtab.c
@@ -151,4 +151,50 @@ struct symtab_entry_s *add_to_symtab(char *symbol)
     return entry;
 }
 
+int rem_from_symtab(struct symtab_entry_s *entry, struct symtab_s *symtab)
+{
+    int res = 0;
+    if(entry->val)
+    {
+        free(entry->val);
+    }
+
+    if(entry->func_body)
+    {
+        free_node_tree(entry->func_body);
+    }
+
+    free(entry->name);
+
+    if(symtab->first == entry)
+    {
+        symtab->first = symtab->first->next;
+
+        if(symtab->last == entry)
+        {
+            symtab->last = NULL;
+        }
+        res = 1;
+    }
+    else
+    {
+        struct symtab_entry_s *e = symtab->first;
+        struct symtab_entry_s *p = NULL;
+
+        while(e && e != entry)
+        {
+            p = e;
+            e = e->next;
+        }
+
+        if(e == entry)
+        {
+            p->next = entry->next;
+            res = 1;
+        }
+    }
+
+    free(entry);
+    return res;
+}
 
