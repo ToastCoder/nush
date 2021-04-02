6c49c2e7e9ce7363dc727c7e322f8d106e9ab66d
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-02 19:02:53 +0530

    Some changes

--- src/prompt.c
+++ src/prompt.c
@@ -2,18 +2,27 @@
 #include "shell.h"
 #include <unistd.h>
 #include <limits.h>
+#include "symtab/symtab.h"
 
 #define CYN   "\x1B[36m"
 #define WHT   "\x1B[37m"
 #define RESET "\x1B[0m"
 
 void print_prompt1(void)
-{
+{   
+    struct symtab_entry_s *entry = get_symtab_entry("PS1");
     char user[PATH_MAX];
     char cwd[PATH_MAX];
     cuserid(user);
     getcwd(cwd, sizeof(cwd));
-    fprintf(stderr, CYN "NUSH@%s>" WHT "%s" CYN "$ " RESET,user,cwd);
+    if(entry && entry->val)
+    {
+        fprintf(stderr,CYN "NUSH@%s>" WHT "%s" CYN "%s" RESET,user,cwd,entry->val);
+    }
+    else
+    {
+        fprintf(stderr, CYN "NUSH@%s>" WHT "%s" CYN "$ " RESET,user,cwd);
+    }
 }
 
 void print_prompt2(void)
